fa92156fcd7faf4f263061421a9d034ffdff77c8
Samuel Lancaster <samuellancaster66@gmail.com>
2020-05-23 16:52:13 -0600

    pushing my latest changes to code

--- db/connection.js
+++ db/connection.js
@@ -1,26 +1,23 @@
-require mysql
+var mysql = require("mysql");
 
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "HYPERDRIVE",
+  database: "employee_trackerDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+});
+
+module.exports = connection;
 
 // connection object
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = connection
---
+++ start.js
@@ -0,0 +1,246 @@
+var connection = require("./db/connection");
+var mysql = require("mysql");
+
+// class to insert data in database
+
+// constructor for connection
+
+this.connection = connection
+const consoleTable = require("console.table");
+const inquirer = require("inquirer");
+
+const printMessage = require('print-message');
+ 
+function validateString(answer) {
+    if (answer != "" && isNaN(parseInt(answer))) {
+        return true;
+    }
+    return false;
+}
+function validateNumber(answer) {
+    if (answer != "" && !isNaN(parseInt(answer))) {
+        return true;
+    }
+    return false;
+}
+
+printMessage([
+    "Employee Tracker",
+    "Follow the prompts to view employees and make changes",
+   
+]);
+
+function init(){
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "init",
+            message: "Select an option:",
+            choices: [
+                "View All Employees",
+                "View All Employess By Role",
+                "View All Employees By Manager",
+                "View All Employees By Department",
+                "Add An Employee",
+                "Add A Department",
+                "Add A Role",
+                "Remove Employee",
+                "Remove Department",
+                "Remove Role",
+                "Update Employee's Role",
+                "Update Employee's Manager",
+                "Update Employee's Department",
+                "View Budget of Department"
+            ]
+        }
+    ]).then(function(answer) {
+        switch (answer.init) {
+            case "View All Employees":
+                viewAll();
+                break;
+            case "View All Employees By Role":
+                viewAllByRole();
+                break;
+            case "View All Employees By Manager":
+                viewAllByManager();
+                break;
+            case "View All Employess By Department":
+                viewAllByDepartment();
+                break;
+            case "Add An Employee":
+                addEmployee();
+                break;
+            case "Add A Department":
+                addDepartment();
+                break;
+            case "Add A Role":
+                addRole();
+                break;
+            case "Remove Employee":
+                removeEmployee();
+                break;
+            case "Remove Department":
+                removeDepartment();
+                break;
+             case "Remove Role":
+                removeRole();
+                break;
+            case "Update Employee's Role":
+                updateEmpRole()
+                break;
+            case "Update Employee's Manager":
+                updateEmpManager()
+                break;
+            case "Update Employee's Department":
+                updateEmpDepartment()
+                break;
+            case "View Budget of Department":
+                viewBudget();
+                 break;
+            
+        }
+    });
+}
+function viewAll(){
+    var query = connection.query("SELECT * FROM employee", function(err, data) {
+         if (err) throw err;
+         console.table(data);
+        init();
+        });
+}
+function addRole() {
+    var query = connection.query("SELECT id, department FROM department", function(err, data) {
+        if (err) throw err;
+        // let choices = data.map(x => `${x.id} - ${x.department}`);
+        let choices = [];
+        for (let i = 0; i < data.length; i++) {
+            choices.push(data[i].id + " - " + data[i].department);
+        }
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "title",
+                message: "Enter the role name:",
+                validate: validateString
+            },
+            {
+                type: "input",
+                name: "salary",
+                message: "Enter the salary:",
+                validate: validateNumber
+            },
+            {
+                type: "list",
+                name: "department",
+                message: "Select the department:",
+                choices: [...choices]
+            }
+        ]).then(function(data) {
+            var arr = data.department.split(" ");
+            var deptID = parseInt(arr[0]);
+            var query = connection.query(`INSERT INTO role (title, salary, department_id) VALUES ('${data.title}', ${data.salary}, ${deptID})`, function(err, data) {
+                if (err) throw err;
+                init();
+            });
+        });
+    });
+}
+function addDepartment() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "department",
+            message: "Enter the department's name:",
+            validate: validateString
+        }
+    ]).then(function(data) {
+        var query = connection.query(`INSERT INTO department (department) VALUES ('${data.department}');`, function(err, data) {
+            if (err) throw err;
+            return data;
+            init();
+    });
+});
+}
+function updateEmp() {
+    const emp = {
+        first_name: "",
+        last_name: "",
+        role_id: 0,
+        manager_id: 0,
+        empID: 0
+    };
+    var query = connection.query("SELECT id, first_name, last_name FROM employee", function(err, data) {
+        if (err) throw err;
+        let choices = data.map(x => `${x.id} - ${x.first_name} ${x.last_name}`);
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "employee",
+                message: "Select an employee:",
+                choices: [...choices]
+            }
+        ]).then(function(data) {
+            var arr = data.employee.split(" ");
+            emp.empID = parseInt(arr[0]);
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name: "firstName",
+                    message: "Enter the employee's first name:",
+                    validate: validateString
+                },
+                {
+                    type: "input",
+                    name: "lastName",
+                    message: "Enter the employee's last name:",
+                    validate: validateString
+                }
+            ]).then(function(data) {
+                emp.first_name = data.firstName;
+                emp.last_name = data.lastName;
+                var query = connection.query("SELECT id, title FROM role", function(err, data) {
+                    if (err) throw err;
+                    let choices = data.map(x => `${x.id} - ${x.title}`);
+                    inquirer.prompt([
+                        {
+                            type: "list",
+                            name: "title",
+                            message: "Select a title:",
+                            choices: [...choices]
+                        }
+                    ]).then(function(data) {
+                        var arr = data.title.split(" ");
+                        emp.role_id = parseInt(arr[0]);
+                        var query = connection.query("SELECT id, first_name, last_name FROM employee", function(err, data) {
+                            if (err) throw err;
+                            let choices = data.map(x => `${x.id} - ${x.first_name} ${x.last_name}`);
+                            choices.push("This employee does not have a manager");
+                            inquirer.prompt([
+                                {
+                                    type: "list",
+                                    name: "manager",
+                                    message: "Select this employee's manager:",
+                                    choices: [...choices]
+                                }
+                            ]).then(function(data) {
+                                if (data.manager === "This employee does not have a manager") {
+                                    emp.manager_id = null;
+                                }
+                                else {
+                                    var arr = data.manager.split(" ");
+                                    emp.manager_id = parseInt(arr[0]);
+                                }
+                                var query = connection.query(`UPDATE employee SET first_name = '${emp.first_name}', last_name = '${emp.last_name}', role_id = ${emp.role_id}, manager_id = ${emp.manager_id} WHERE id = ${emp.empID}`, function(err, data) {
+                                    if (err) throw err;
+                                    init();
+                                    return data;
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+}
+init();
